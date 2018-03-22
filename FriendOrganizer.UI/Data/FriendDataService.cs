92402168276227e529b1e66ad3bdf0a611489a8d
Sergey Mishin <smishin91@yandex.ru>
2018-03-22 22:48:32 +0300

    update

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,16 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.Generic;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            yield return new Friend { FirstName = "Сергей", LastName = "Мишин" };
+            yield return new Friend { FirstName = "Анджелина", LastName = "Джоли" };
+            yield return new Friend { FirstName = "Брэд", LastName = "Питт" };
+            yield return new Friend { FirstName = "Жерар", LastName = "Пике" };
+        }
+    }
+}
---
+++ FriendOrganizer.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using FriendOrganizer.Model;
+using FriendOrganizer.UI.Data;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendOrganizer.UI.ViewModel
+{
+    public class MainViewModel
+    {
+        private IFriendDataService _friendDataService;
+
+        public MainViewModel(IFriendDataService friendDataService)
+        {
+            Friends = new ObservableCollection<Friend>();
+            _friendDataService = friendDataService;
+        }
+
+        public void Load()
+        {
+            var friends = _friendDataService.GetAll();
+            Friends.Clear();
+            foreach (var friend in friends)
+            {
+                Friends.Add(friend);
+            }
+        }
+
+        public ObservableCollection<Friend> Friends { get; set; }
+
+        private Friend selectedFriend;
+
+        public Friend SelectedFriend
+        {
+            get { return selectedFriend; }
+            set { selectedFriend = value; }
+        }
+
+    }
+}
